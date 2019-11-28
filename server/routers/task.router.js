d1f4dd0c42fef2e8f138ee3934d42e99e581e8bc
Dane Schmidt <daneschmidt@gmail.com>
2019-11-28 14:01:33 -0600

    pool is setup and connected to the db

--- server/routers/task.router.js
+++ server/routers/task.router.js
@@ -2,9 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 
-
-const taskArray = [];
-
 router.get('/', (req, res) => {
     res.send(200);
 
--- server/server.js
+++ server/server.js
@@ -3,6 +3,27 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 
+const pg = require('pg');
+const Pool = pg.Pool;
+const pool = new Pool({
+    database: "weekend-to-do-app",
+    host: "localhost",
+    port: 5432,
+    max: 10000000,    // ;)
+    idleTimeoutMillis: 30000
+});
+
+pool.on('connect', () => {
+    console.log('database is good boss');
+
+});
+
+pool.on('error', (error) => {
+    console.log(`OH SHIT pool is broken' ${error}`);np
+
+});
+
+
 //ROUTES
 const taskRouter = require('./routers/task.router');
 
@@ -10,10 +31,11 @@ app.use(express.static('server/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-//ROUTES
-app.use('/api/tasks', taskRouter);
+// //ROUTES
+// app.use('/api/tasks', taskRouter);
 
 
 app.listen(PORT, () => {
+    console.log(pool.query(`SELECT * FROM "tasklist"`));
     console.log(`Server is up on port: ${PORT}`);
 })
