698820915aa87097f12d4b37d8efbbfcf0e2f554
Dane Schmidt <daneschmidt@gmail.com>
2019-12-03 19:15:07 -0600

    safe-reaches-11900 setting up Heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,12 +1,34 @@
 const pg = require('pg');
-const Pool = pg.Pool;
-const pool = new Pool({
-    database: "weekend-to-do-app",
-    host: "localhost",
-    port: 5432,
-    max: 10000000,    // ;)
-    idleTimeoutMillis: 30000
-});
+const url = require('url');
+
+let config = {};
+
+if (process.env.DATABASE_URL) {
+    const params = url.parse(process.env.DATABASE_URL);
+    const auth = params.auth.split(':');
+
+    config = {
+        user: auth[0],
+        password: auth[1],
+        host: params.hostname,
+        port: params.port,
+        database: params.pathname.split('/')[1],
+        ssl: true,
+        max: 10,
+        idleTimeoutMillis: 30000
+    }
+} else {
+    config = {
+        host: 'localhost',
+        port: 5432,
+        database: 'weekend-to-do-app',
+        max: 10,
+        idleTimeoutMillis: 30000,
+    }
+}
+
+const pool = new pg.Pool(config);
+
 
 pool.on('connect', () => {
     console.log('database is good boss');
@@ -15,5 +37,16 @@ pool.on('connect', () => {
 pool.on('error', (error) => {
     console.log(`OH SHIT pool is broken' ${error}`);
 });
+
+
+// const Pool = pg.Pool;
+// const pool = new Pool({
+//     database: "weekend-to-do-app",
+//     host: "localhost",
+//     port: 5432,
+//     max: 10,    // ;)
+//     idleTimeoutMillis: 30000
+// });
+
 
 module.exports = pool;
